9c97da5ec26751b73abcf3dc7a62eb03d9dc7b3d
Mucaca <Mastnyvlas@gmail.com>
2025-06-16 20:05:39 +0200

    Basic electron project
    - Rollback actual demo

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,15 +1,4 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+
 // https://vitejs.dev/config
-export default defineConfig({
-  optimizeDeps: {
-    exclude: [
-      "@sqlite.org/sqlite-wasm",
-      "kysely",
-      "@evolu/common",
-      "@evolu/react",
-      "@evolu/react-web",
-    ],
-  },
-  plugins: [react()],
-});
+export default defineConfig({});
